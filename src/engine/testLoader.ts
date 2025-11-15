7b9206d723f1cee39c3e96d6ed9f41337925e1eb
farmble <dev@farmble.xyz>
2025-11-15 22:54:04 +0900

    feat: young-forty test whitelist

--- src/engine/testLoader.ts
+++ src/engine/testLoader.ts
@@ -3,16 +3,23 @@ import relationshipVibes from '../../assets/tests/relationship_vibes_01.json';
 import studyFocus from '../../assets/tests/study_focus_test_01.json';
 import { PersonalityTest } from './testTypes';
 
-const tests: PersonalityTest[] = [
+const ALL_TESTS: PersonalityTest[] = [
   burnoutTest as PersonalityTest,
   relationshipVibes as PersonalityTest,
   studyFocus as PersonalityTest,
+  // TODO: 필요 시 추가 JSON을 여기에 push
+];
+
+const YOUNG_FORTY_TEST_IDS: string[] = [
+  'burnout_test_01',
+  'relationship_vibes_01',
+  // TODO: 영포티 타겟 테스트 추가 시 id를 여기에 등록
 ];
 
 export const getAllTests = (): PersonalityTest[] => {
-  return tests;
+  return ALL_TESTS.filter((test) => YOUNG_FORTY_TEST_IDS.includes(test.id));
 };
 
 export const getTestById = (id: string): PersonalityTest | undefined => {
-  return tests.find((test) => test.id === id);
+  return ALL_TESTS.find((test) => test.id === id);
 };
